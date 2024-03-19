9ec51704929edeb56dd4462d3c5a9c28d6ad3e9f
Ashraf Sharaf <ashrafsharafcs@gmail.com>
2024-03-19 20:18:01 +0200

    creating API to get the number of bookings

---
+++ backend/adminTotalBookings.php
@@ -0,0 +1,15 @@
+<?php
+include('connection.php');
+
+$query=$mysqli->prepare("SELECT COUNT(*) AS totalBookings FROM bookings");
+$query->execute();
+$query->bind_result($totalBookings);
+$query->fetch();
+if($totalBookings ==0){
+    $response["status"] = "No Bookings Found";
+}else{
+    $totalBookings;
+    $response["status"] = "Bookings Found";
+    $response["result"] =  $totalBookings;
+}
+echo json_encode($response);
